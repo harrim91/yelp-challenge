9527f22717aad3233c7c1219199ec98ac9e26921
Michael Harrison <harrim91@hotmail.co.uk>
2016-05-25 17:04:47 +0100

    edit and update restaurants, converted from erb to haml

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,15 +10,32 @@ class RestaurantsController < ApplicationController
   def new
   end
 
+  def edit
+    @restaurant = Restaurant.find params[:id]
+  end
+
   def create
-    Restaurant.create(restaurant_params)
-    redirect_to '/restaurants'
+    Restaurant.create restaurant_params
+    redirect_to restaurants_path
+  end
+
+  def update
+    Restaurant.find(params[:id])
+      .update restaurant_params
+    redirect_to restaurants_path
+  end
+
+  def destroy
+    Restaurant.find(params[:id])
+      .destroy
+    flash[:notice] = 'Restaurant deleted'
+    redirect_to restaurants_path
   end
 
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :rating)
+    params.require(:restaurant).permit(:name, :description)
   end
 
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,28 +1,12 @@
 require 'rails_helper'
 
 feature 'restaurants' do
+
   context 'no restaurants have been added' do
 
     scenario 'displays message when no restaurants have been added' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants found'
-      expect(page).to have_link 'Add Restaurant'
-    end
-
-  end
-
-  context 'restaurants have been added' do
-    before do
-      Restaurant.create name:'Michael\'s Mac Shack', rating: 5
-    end
-
-    scenario 'displays the restaurant' do
-      visit '/restaurants'
-      within 'table' do
-        expect(page).to have_content 'Michael\'s Mac Shack'
-        expect(page).to have_content '5'
-      end
-      expect(page).not_to have_content 'No restaurants found'
       expect(page).to have_link 'Add Restaurant'
     end
 
@@ -35,33 +19,76 @@ feature 'restaurants' do
       click_link 'Add Restaurant'
       expect(current_path).to eq '/restaurants/new'
       fill_in :Name, with: 'Macey\'s Meatballs'
-      fill_in :Rating, with: 5
+      fill_in :Description, with: 'That\'s a SPICY meat-a-ball'
       click_button 'Add'
       expect(current_path).to eq '/restaurants'
       within 'table' do
         expect(page).to have_content 'Macey\'s Meatballs'
-        expect(page).to have_content '5'
+        expect(page).to have_content 'That\'s a SPICY meat-a-ball'
       end
     end
 
   end
 
-  context 'viewing a restaurant' do
-    let!(:mmc) { Restaurant.create name:'Michael\'s Mac Shack', rating: 5 }
+  context 'restaurants have been added' do
+    let!(:mms) do
+      Restaurant.create name:'Michael\'s Mac Shack',
+                        description: 'Cheesin\''
+    end
 
-    scenario 'lets a user view an inidividual restaurant' do
+    scenario 'index displays the restaurant' do
       visit '/restaurants'
       within 'table' do
-        click_link 'Michael\'s Mac Shack'
-      end
-      expect(current_path).to eq "/restaurants/#{mmc.id}"
-      within 'h1' do
         expect(page).to have_content 'Michael\'s Mac Shack'
+        expect(page).to have_content 'Cheesin\''
       end
-      within 'h2' do
-        expect(page).to have_content 'Rating: 5'
-      end
+      expect(page).not_to have_content 'No restaurants found'
+      expect(page).to have_link 'Add Restaurant'
     end
+
+    context 'viewing a restaurant' do
+
+      scenario 'lets a user view a restaurant' do
+        visit '/restaurants'
+        within('table') { click_link 'Michael\'s Mac Shack' }
+        expect(current_path).to eq "/restaurants/#{mms.id}"
+        within('h1') { expect(page).to have_content 'Michael\'s Mac Shack' }
+        within ('p') { expect(page).to have_content 'Cheesin\'' }
+      end
+
+    end
+
+    context 'editing a restaurant' do
+
+      scenario 'lets user edit a restaurant' do
+        visit "/restaurants/#{mms.id}"
+        click_link 'Edit'
+        expect(current_path).to eq "/restaurants/#{mms.id}/edit"
+        fill_in :Name, with: 'Mike\'s Mac Shack'
+        fill_in :Description, with: 'Super Cheese'
+        click_button 'Update'
+        expect(current_path).to eq '/restaurants'
+        within 'table' do
+          expect(page).to have_content 'Mike\'s Mac Shack'
+          expect(page).to have_content 'Super Cheese'
+        end
+      end
+
+    end
+
+    context 'deleting a restaurant' do
+
+      scenario 'lets user delete a restaurant' do
+        visit "/restaurants/#{mms.id}"
+        click_link 'Delete'
+        expect(current_path).to eq '/restaurants'
+        expect(page).to have_content 'Restaurant deleted'
+        expect(page).to have_content 'No restaurants found'
+        expect(page).not_to have_content 'Michael\'s Mac Shack'
+      end
+
+    end
+
   end
 
 end
